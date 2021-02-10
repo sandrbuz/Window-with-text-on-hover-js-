91cd184cef0f4991166dc02fd090ebe773b16ec7
sandrbuz <sandrbuz@mail.ru>
2021-02-10 23:30:23 +0300

    window on hover

---
+++ main.js
@@ -0,0 +1,24 @@
+var btn = document.getElementById('wrapper');
+var badge = document.getElementById('badge');
+var wrapper = document.getElementById('wrapper');
+var text = document.getElementById('text');
+
+btn.addEventListener('mouseover', function open() {
+    badge.classList.add('badge1');
+    wrapper.classList.add('wrapper1');
+    text.classList.add('text1');
+
+});
+
+
+
+btn.addEventListener('mouseout', function close() {
+    badge.classList.remove('badge1');
+    wrapper.classList.remove('wrapper1');
+    text.classList.remove('text1');
+}
+);
+
+
+
+
